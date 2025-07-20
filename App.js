3496ede7d7a858bf6ab3bd77443ed1bc78b8c0e4
manoo25 <mohamedsalamma25@gmail.com>
2025-07-20 13:40:26 +0300

    Download App

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <TextInput placeholder='name' style={{backgroundColor:'orange',width:'90%',color:'white',borderRadius:12}}/>
+      <AntDesign name="stepforward" size={24} color="black" />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightgreen',
     alignItems: 'center',
     justifyContent: 'center',
   },
