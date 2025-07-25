a5136e6acda5e6790bc439ff1a2ca31f31fb1dab
Tarek Hegazy <tarek95506@gmail.com>
2025-07-25 16:03:44 +0300

    SoftDrinks and Compaines

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import usersSlice from "./Slices/users";
+import companiesReducer from "./Slices/companiesSlice";
+import productsSlice from "./Slices/productsSlice";
 
 
-export  const store=configureStore({
-    reducer:{
-        Users: usersSlice.reducer,
-    }
+export  const store = configureStore({
+  reducer: {
+    Users: usersSlice.reducer,
+    companies: companiesReducer.reducer,
+    products: productsSlice.reducer,
+  },
 });
