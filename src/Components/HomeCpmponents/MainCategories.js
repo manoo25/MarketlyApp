b04a9ff7052fb6a5ad3f1d9742ba861774bbf081
manoo25 <mohamedsalamma25@gmail.com>
2025-07-27 17:48:15 +0300

    Save local changes before merging Rasha

--- src/Components/HomeCpmponents/MainCategories.js
+++ src/Components/HomeCpmponents/MainCategories.js
@@ -1,65 +1,28 @@
 import { FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import AntDesign from "@expo/vector-icons/AntDesign";
-import LabelInpts from "../GlobalComponents/LabelInpts";
-import { styles } from "../../../styles";
 import SectionHeader from "../GlobalComponents/SectionHeader";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchcategories } from "../../Redux/Slices/Categories";
 import { useEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { PATHS } from "../../routes/Paths";
+import { styles } from "../../../styles";
 
-const MoreObj = 
-    { id: '6', name: 'المزيد',  img: require("../../../assets/HomeSlide/more.png") }
-
-
-
-// const CategoryCard = ({ item }) => (
-//     <View style={componentStyles.cardContainer}>
-//         <TouchableOpacity>
-//             <View style={componentStyles.imageContainer}>
-//           {
-//              item.id=='6'?
-//             <Image  source={item.img} style={componentStyles.image} 
-//             resizeMode="contain"
-//             />
-//             :
-//              <Image  source={{ uri: item.img }} style={componentStyles.image} 
-//             resizeMode="contain"
-//             />
-//           }
-//         </View>
-//         <View >
-//             <Text style={[componentStyles.titleText, styles.h2,{fontSize:11}]}>{item.name}</Text>
-           
-//         </View>
-//         </TouchableOpacity>
-//     </View>
-// );
+const MoreObj = { id: '6', name: 'المزيد', img: require("../../../assets/HomeSlide/more.png") };
 
 const CategoryCard = ({ item, onPress }) => (
   <View style={componentStyles.cardContainer}>
-    <TouchableOpacity onPress={onPress}>
+    <TouchableOpacity onPress={onPress} activeOpacity={0.7}>
       <View style={componentStyles.imageContainer}>
         {item.id == "6" ? (
-          <Image
-            source={item.img}
-            style={componentStyles.image}
-            resizeMode="contain"
-          />
+          <Image source={item.img} style={componentStyles.image} resizeMode="contain" />
         ) : (
-          <Image
-            source={{ uri: item.img }}
-            style={componentStyles.image}
-            resizeMode="contain"
-          />
+          <Image source={{ uri: item.img }} style={componentStyles.image} resizeMode="contain" />
         )}
       </View>
-      <View>
-        <Text style={[componentStyles.titleText, styles.h2, { fontSize: 11 }]}>
-          {item.name}
-        </Text>
-      </View>
+      <Text style={[componentStyles.titleText, styles.h2, { fontSize: 11 }]}>
+        {item.name}
+      </Text>
     </TouchableOpacity>
   </View>
 );
@@ -67,8 +30,8 @@ const CategoryCard = ({ item, onPress }) => (
 function MainCategories() {
   const dispatch = useDispatch();
   const { categories } = useSelector((state) => state.Categories);
-  const [MoreCat, SetMoreCat] = useState([]);
-  const navigation = useNavigation(); // ✅
+  const [displayCategories, setDisplayCategories] = useState([]);
+  const navigation = useNavigation();
 
   useEffect(() => {
     dispatch(fetchcategories());
@@ -76,78 +39,76 @@ function MainCategories() {
 
   useEffect(() => {
     if (categories.length > 0) {
-      const TargetData = categories.slice(0, 5);
-      SetMoreCat([...TargetData, MoreObj]);
+      const initialCategories = categories.slice(0, 5);
+      setDisplayCategories([...initialCategories, MoreObj]);
     }
   }, [categories]);
 
   const handleCategoryPress = (item) => {
     if (item.id === "6") {
-      navigation.navigate(PATHS.AllCategories); // ✅ روح لصفحة المزيد
+      navigation.navigate(PATHS.AllCategories);
     } else {
-      navigation.navigate(PATHS.CategoryProducts, { category: item });// هنكملها بعدين
+      navigation.navigate(PATHS.CategoryProducts, { category: item });
     }
   };
 
   return (
-    <View style={{ height: "auto" }}>
-      <SectionHeader text=" الأقسام" />
-
+    <View style={componentStyles.container}>
+      <SectionHeader text="الأقسام" />
+      
       <FlatList
-        horizontal
-        inverted
-        scrollEnabled={false}
-        showsHorizontalScrollIndicator={false}
-        data={MoreCat}
+        data={displayCategories}
         renderItem={({ item }) => (
           <CategoryCard item={item} onPress={() => handleCategoryPress(item)} />
         )}
         keyExtractor={(item) => item.id}
+        numColumns={4} 
+        columnWrapperStyle={componentStyles.columnWrapper}
         contentContainerStyle={componentStyles.listContentContainer}
+        scrollEnabled={false}
+        showsVerticalScrollIndicator={false}
+        showsHorizontalScrollIndicator={false}
       />
     </View>
   );
 }
 
-
 const componentStyles = StyleSheet.create({
-    listContentContainer: {
-        flex:1,
-        flexDirection:'row',
-      
-        paddingHorizontal:16,
-        flexWrap:'wrap',
-        gap:16,
-        marginTop:5,
-        height:'auto'
-        
-     
-    },
-    cardContainer: {
-      
-        width: 77,
-        height:82,
-       flexDirection:'column',
-       alignItems:'center',
-    },
-    imageContainer: {
-        height: 60,
-        width: 60,
-        borderRadius: 10,
-        backgroundColor: '#327AFF1A',
-    },
-    image: {
-        width: '100%',
-        height: '100%',
-        borderRadius: 10,
-    },
-
-    titleText: {
-        textAlign: 'center',
-        marginTop: 6,
-
-    },
-   
+  container: {
+    paddingHorizontal: 16,
+    marginBottom: 20,
+  },
+  listContentContainer: {
+    paddingTop: 10,
+  },
+  columnWrapper: {
+    flexDirection: 'row-reverse',
+    justifyContent: 'flex-start',
+    marginBottom: 16,
+  },
+  cardContainer: {
+    width: 77,
+    height: 82,
+    alignItems: 'center',
+    marginHorizontal: 8,
+  },
+  imageContainer: {
+    height: 60,
+    width: 60,
+    borderRadius: 10,
+    backgroundColor: '#327AFF1A',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: '80%',
+    height: '80%',
+  },
+  titleText: {
+    textAlign: 'center',
+    marginTop: 6,
+    color: '#424047',
+  },
 });
 
 export default MainCategories;
