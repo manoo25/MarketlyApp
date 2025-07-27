b8e1b49024f806612aef1ecfc1ca593577abeb6e
Mohamed Salama <117160810+manoo25@users.noreply.github.com>
2025-07-27 18:23:02 +0300

    Delete src directory

--- src/Redux/Slices/GetUserData.js
+++
@@ -1,7 +0,0 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-export let UserId;
-export const UserData = async () => {
-  const data = await AsyncStorage.getItem("userData");
-  UserId=data&&JSON.parse(data).id;
-  return data && JSON.parse(data) ;
-};
